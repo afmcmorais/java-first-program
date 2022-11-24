5a5ab8b81a64b9573a98a1eecbfc1a05086f89d4
André Felipe <afmcmorais@icloud.com>
2022-11-24 17:22:46 -0300

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,13 +6,12 @@ package com.h2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return 2 * -1;
+        return 2 * number;
     }
 
     private static int add(int[] numbers) {
